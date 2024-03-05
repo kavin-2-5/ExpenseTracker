b3a20611ccdc7d8db45e0849eacd4fac5a84d1df
kavin-2-5 <kavin0133@gmail.com>
2024-03-05 14:01:02 +0530

    expense_tracker

--- index.js
+++ index.js
@@ -2,39 +2,12 @@ const bodyParser = require('body-parser')
 const express = require('express')
 const mongoose = require('mongoose')
 const { Expense } = require('./schema.js')
+const cors = require('cors');
 
 const app = express()
 app.use(bodyParser.json())
-/**
- * git clone <link>
- * 
- * (add .gitignore file)
- * 
- * git add .
- * git commit -m "any msg"
- * git push origin main
- * 
- * git config --global user.name '<username>'
- * git config --global user.email <emailId>
- */
+app.use(cors())
 
-/**
- * Expense Tracker
- * 
- * Features and end points
- * 
- * Adding a new expense/income : /add-expense -> post
- * Displaying existing expenses : /get-expenses -> get
- * Editing existing entries : /edit-expense -> patch/put
- * Deleting expenses : /delete-expense -> delete
- * 
- * Budget reporting
- * Creating new user
- * Validating user
- * 
- * Defining schema
- * category, amount, date 
- */
 
 async function connectToDb() {
     try {
@@ -42,7 +15,7 @@ async function connectToDb() {
 
 
         console.log('DB connection established :)')
-        const port = 8000
+        const port = process.env.PORT ||8000
         app.listen(port, function() {
             console.log(`Listening on port ${port}...`)
         })
@@ -68,6 +41,71 @@ app.post('/add-expense', async function(request, response) {
         response.status(500).json({
             "status" : "failure",
             "message" : "entry not created",
+            "error" : error
+        })
+    }
+})
+
+app.get('/get-expenses', async function(request, response) {
+    try {
+        const expensesData = await Expense.find()
+        response.status(200).json(expensesData)
+    } catch(error) {
+        response.status(500).json({
+            "status" : "failure",
+            "message" : "could not fetch entries",
+            "error" : error
+        })
+    }
+})
+
+app.delete('/delete-expense/:id', async function(request, response) {
+    try {
+        const expenseData = await Expense.findById(request.params.id)
+        if(expenseData) {
+            await Expense.findByIdAndDelete(request.params.id)
+            response.status(200).json({
+                "status" : "success",
+                "message" : "deleted entry"
+            })
+        } else {
+            response.status(404).json({
+                "status" : "failure",
+                "message" : "could not find entry"
+            })
+        }
+    } catch(error) {
+        response.status(500).json({
+            "status" : "failure",
+            "message" : "could not delete entry",
+            "error" : error
+        })
+    }
+})
+
+app.patch('/edit-expense/:id', async function(request, response) {
+    try {
+        const expenseEntry = await Expense.findById(request.params.id)
+        if(expenseEntry) {
+            await expenseEntry.updateOne({
+                "amount" : request.body.amount,
+                "category" : request.body.category,
+                "date" : request.body.date
+            })
+            response.status(200).json({
+                "status" : "success",
+                "message" : "updated entry"
+            })
+        } else {
+            response.status(404).json({
+                "status" : "failure",
+                "message" : "could not find enter"
+            })
+        }
+    } catch(error) {
+        response.status(500).json({
+            "status" : "failure",
+            "message" : "could not delete entry",
             "error" : error
         })
     }
